31c350f01194f582f223881ebf513b0770e1e591
Reena Gouldbourne <reenag@microsoft.com>
2022-09-09 18:38:41 -0400

    first commit

--- script.js
+++ script.js
@@ -14,11 +14,13 @@ const sunrise = document.getElementById('sunrise');
 const sunset = document.getElementById('sunset');
 const forecastDiv = document.getElementById('forecastDiv');
 
+
 const week = ['Sun','Mon','Tue','Wed', 'Thu','Fri','Sat'];
 const monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
 const houstonLonLat = [-95.358421, 29.749907];
 const apiKey = 'ee759a30a9b8bfdc78fd32c59d9d8abc';
 const lngLatSearch = [];
+var imageUrl = "";
 
 
 
@@ -34,7 +36,7 @@ async function getWeather(longLat) {
         currentHumidity.textContent = weatherData.main.humidity + "%";
         currentWeatherDes.textContent = weatherData.weather[0].description.toUpperCase();
         currentWeatherImg.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-
+        
         //get unix time format to dateTime format for sunset and sunrise
         const sunriseDateTime = new Date(weatherData.sys.sunrise*1000);
         const sunsetDateTime = new Date(weatherData.sys.sunset*1000);
@@ -46,26 +48,26 @@ async function getWeather(longLat) {
 }
 
 async function getForecast(longLat){
- try {
-    const forecastPromise = await fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${longLat[1]}&lon=${longLat[0]}&appid=${apiKey}&units=imperial`);
-    const forecastObject = await forecastPromise.json();
-    const forecastData = forecastObject.list;
-
-    const forecastHTML = [];
-
-    for (let i = 0; i < forecastData.length; i++) {
-        const eachElement = `<div class="d-flex flex-column me-5">
-        <img class="" src="http://openweathermap.org/img/wn/${forecastData[i].weather[0].icon}@2x.png" alt="" width="40" height="40">
-        <p style="color: #000d63;">${forecastData[i].main.temp.toString().substr(0,2)}°</p>
-        <p>${forecastData[i].dt_txt.substr(10,6)}</p>
-        <h5>${getMonthDate(forecastData[i].dt)}</h5>
-        </div>`;
-        forecastHTML.push(eachElement);
+    try {
+        const forecastPromise = await fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${longLat[1]}&lon=${longLat[0]}&appid=${apiKey}&units=imperial`);
+        const forecastObject = await forecastPromise.json();
+        const forecastData = forecastObject.list;
+        
+        const forecastHTML = [];
+        
+        for (let i = 0; i < forecastData.length; i++) {
+            const eachElement = `<div class="d-flex flex-column me-5">
+            <img class="" src="http://openweathermap.org/img/wn/${forecastData[i].weather[0].icon}@2x.png" alt="" width="40" height="40">
+            <p style="color: #000d63;">${forecastData[i].main.temp.toString().substr(0,2)}°</p>
+            <p>${forecastData[i].dt_txt.substr(10,6)}</p>
+            <h5>${getMonthDate(forecastData[i].dt)}</h5>
+            </div>`;
+            forecastHTML.push(eachElement);
+        }
+        forecastDiv.innerHTML = forecastHTML.join('');
+    } catch (error) {
+        
     }
-    forecastDiv.innerHTML = forecastHTML.join('');
- } catch (error) {
-    
- }
 }
 
 function getMonthDate(unixDateTime){
@@ -83,16 +85,17 @@ function kelvinToFahr(temp){
 
 function formattedTime(dateTime){
     var minutes = "0" + dateTime.getHours();
-
+    
     var seconds = "0" + dateTime.getMinutes();
-
-
+    
+    
     return minutes.substr(-2) + ':' + seconds.substr(-2);
 }
 
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
+    document.getElementById("img-container").style.backgroundImage="url('icons/houston.jpeg')"
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((loc) => {
             lngLatSearch[0] = loc.coords.longitude;
@@ -105,17 +108,17 @@ window.addEventListener('DOMContentLoaded', (e) => {
         lngLatSearch[1] = 29.749907;
         getWeather(lngLatSearch);
         getForecast(lngLatSearch);
-
     }
-    
+
+    initMap();
+   
 });
 
-
-function initMap(){
-    autocomplete = new  google.maps.places.Autocomplete(locationInput, 
+async function initMap(){
+    autocomplete = await new  google.maps.places.Autocomplete(locationInput, 
     {
         componentRestrictions: {'country': ['us']},
-        fields: ['geometry','name','address_components'],
+        fields: [''],
         types: ['geocode']
     })
 
@@ -123,16 +126,21 @@ function initMap(){
         const location = autocomplete.getPlace();
         lngLatSearch[0] = location.geometry.location.lng();
         lngLatSearch[1] = location.geometry.location.lat();
-
+        imageUrl = location.photos[0].getUrl()
+        locationImgText.textContent = location.name;
     })
     searchBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        locationImgText.textContent = location.name;
         getWeather(lngLatSearch);
         getForecast(lngLatSearch);
+        updateImage(imageUrl);
     })
 
 }
-initMap();
+
+function updateImage(imageUrl){
+    document.getElementById("img-container").style.backgroundImage=`url(${imageUrl})`
+}
+
 
 
